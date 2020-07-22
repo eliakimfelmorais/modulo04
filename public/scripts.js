c229fbe62df3a5a3eb298087821578a6d319836f
Eliakim Feliciano Morais <eliakimfelmorais@gmail.com>
2020-07-21 21:45:10 -0300

    adicao de view para membros

--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,30 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const cards  = document.querySelectorAll('.card');
+// const modalOverlay = document.querySelector('.modal-overlay');
+// const cards  = document.querySelectorAll('.card');
 
-for (let card of cards){
-    card.addEventListener("click", function(){
-        const videoId = card.getAttribute("id");
-        window.location.href = `/video/?id=${videoId}`;
-    })
+// for (let card of cards){
+//     card.addEventListener("click", function(){
+//         const videoId = card.getAttribute("id");
+//         window.location.href = `/video/?id=${videoId}`;
+//     })
+// }
+
+// document.querySelector('.close-modal').addEventListener("click", function(){
+//     modalOverlay.classList.remove('active');
+//     modalOverlay.querySelector("iframe").src="";
+// })
+
+
+
+const currentPage = location.pathname;
+
+
+
+
+const menuItems = document.querySelectorAll("header .links a");
+
+for (item of menuItems){
+    
+    if(currentPage.includes(item.getAttribute("href"))){
+        item.classList.add("active");
+    }
 }
-
-document.querySelector('.close-modal').addEventListener("click", function(){
-    modalOverlay.classList.remove('active');
-    modalOverlay.querySelector("iframe").src="";
-})
-
